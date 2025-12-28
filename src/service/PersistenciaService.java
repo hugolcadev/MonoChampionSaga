8fee6e7b9dd1d6bfe95e46835b8e7a56d83d1e8c
Hugo Luis <hugolcabreu@gmail.com>
2025-12-28 01:22:19 -0300

    Implementação do Gson

---
+++ src/service/PersistenciaService.java
@@ -0,0 +1,12 @@
+package service;
+
+import com.google.gson.Gson;
+import java.io.FileWriter;
+import java.io.FileReader;
+import java.io.IOException;
+
+import model.Campeao;
+
+public class PersistenciaService{
+
+}
